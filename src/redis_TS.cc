a83f2a19eb521653b0a05a2e6f99f75b7c27328a
zhaoxiaobiao <zhaoxiaobiaogm@gmail.com>
2022-07-13 22:52:48 +0800

    ✨ feat: update ts range

--- src/redis_TS.cc
+++ src/redis_TS.cc
@@ -29,7 +29,7 @@ rocksdb::Status TS::MAdd(const std::string primary_key,
   rocksdb::WriteBatch batch;
   int64_t now;
   rocksdb::Env::Default()->GetCurrentTime(&now);
-  LockGuard guard(storage_->GetLockManager(), primary_key);
+  // LockGuard guard(storage_->GetLockManager(), primary_key);
   for (const auto &pair : pairs) {
     std::string ns_key;
     std::string bytes;
@@ -46,7 +46,7 @@ rocksdb::Status TS::MAdd(const std::string primary_key,
     std::string combination_key =
         pair.primary_key + "~" + pair.clustering_id + "~" + pair.timestamp;
     AppendNamespacePrefix(combination_key, &ns_key);
-    batch.Put(ns_key, bytes);
+    batch.Put(metadata_cf_handle_, ns_key, bytes);
   }
   auto s = storage_->Write(rocksdb::WriteOptions(), &batch);
   if (!s.ok()) return s;
@@ -58,10 +58,31 @@ rocksdb::Status TS::Add(const std::string primary_key, TSPairs &pair) {
   return MAdd(primary_key, pairs);
 }
 
+static std::string MakePrefixKey(TSPairs &pair) {
+  std::string prefix_key;
+  prefix_key.append(pair.primary_key);
+  if (pair.clustering_id.empty()) {
+    return prefix_key;
+  }
+
+  prefix_key.append("~");
+  if ("*" == pair.clustering_id.substr(pair.clustering_id.length() - 1)) {
+    prefix_key.append(
+        pair.clustering_id.substr(0, pair.clustering_id.length() - 1));
+  } else {
+    prefix_key.append(pair.clustering_id).append("~");
+  }
+
+  return prefix_key;
+}
+
+static std::string FilterValue(const std::string &ori_value) {
+  return ori_value.substr(TS_HDR_SIZE);
+}
+
 rocksdb::Status TS::Range(TSPairs &pair, std::vector<TSFieldValue> *values) {
   std::string ns_key;
-  std::string prefix_key = "";
-  prefix_key += pair.primary_key;
+  std::string prefix_key = MakePrefixKey(pair);
 
   AppendNamespacePrefix(prefix_key, &ns_key);
 
@@ -69,17 +90,24 @@ rocksdb::Status TS::Range(TSPairs &pair, std::vector<TSFieldValue> *values) {
   rocksdb::ReadOptions read_options;
   read_options.snapshot = ss.GetSnapShot();
   read_options.fill_cache = false;
-  auto iter = DBUtil::UniqueIterator(db_, rocksdb::ReadOptions());
+  auto iter =
+      DBUtil::UniqueIterator(db_, rocksdb::ReadOptions(), metadata_cf_handle_);
   for (iter->Seek(ns_key); iter->Valid(); iter->Next()) {
     if (!iter->key().starts_with(ns_key)) {
       break;
+    }
+
+    Metadata metadata(kRedisNone, false);
+    metadata.Decode(iter->value().ToString());
+    if (metadata.Expired()) {
+      continue;
     }
     std::string ns;
     std::string user_key;
     ExtractNamespaceKey(iter->key(), &ns, &user_key,
                         storage_->IsSlotIdEncoded());
-    values->emplace_back(TSFieldValue{
-        user_key, "0", iter->value().ToString().substr(TS_HDR_SIZE)});
+    values->emplace_back(
+        TSFieldValue{user_key, "0", FilterValue(iter->value().ToString())});
   }
   return rocksdb::Status::OK();
 }
